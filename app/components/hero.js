2c63c75354cea4509b1663ceabdd2747566db0eb
Shane F. Enriquez <96838519+shanepai-14@users.noreply.github.com>
2024-02-24 02:06:50 +0800

    add

--- app/components/hero.js
+++ app/components/hero.js
@@ -4,11 +4,11 @@ import deped from "@/public/images/DepEd BG.png";
 import Image from "next/image";
 const hero = () => {
   return (
-    <section className="h-screen relative">
+    <section className="h-screen relative hero">
       <div></div>
 
       <div className="z-10">
-        <h1>Republic of the Philippines</h1>
+        <h1 className="text-6xl">Republic of the Philippines</h1>
       </div>
     </section>
   );
